87bfc5f7ad86415da710eb5a264c0f95c95c2f3a
Rahul <grovyghost@protonmail.com>
2024-06-07 14:15:36 +0530

    removed loadlinux

--- vars/loadLinuxScript.groovy
+++
@@ -1,5 +0,0 @@
-def call(Map config = [:]) { 
-  def scriptcontents = libraryResource "${config.name}"    
-  writeFile file: "${config.name}", text: scriptcontents 
-  sh "chmod a+x ./${config.name}"
-} 
