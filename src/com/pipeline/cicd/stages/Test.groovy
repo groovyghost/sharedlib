23ac3191bcac1a3d631e2433b81573f5a6c286af
Rahul <grovyghost@protonmail.com>
2024-06-10 16:07:10 +0530

    working

--- src/com/pipeline/cicd/stages/Test.groovy
+++ src/com/pipeline/cicd/stages/Test.groovy
@@ -33,30 +33,4 @@ void execute() {
 		}
 	}
 }
-		// @Override
-		// void execute() {
-		// 	script.stage(stageName) {
-		// 		script.node(Constant.NODE) {
-		// 			def deployChoice = script.input(
-		// 					id: 'approve_for_deployment',
-		// 					message: 'Approve for deployment',
-		// 					ok: 'Proceed',
-		// 					parameters: [script.choice(
-		// 							name: 'Release to deploy?',
-		// 							choices: 'Yes\nNo',
-		// 							description: 'NO - Build will not be deployed in the stage server')],
-		// 					submitter: 'rahul'
-		// 			)
-
-		// 			if (deployChoice.contains("Yes")) {
-		// 				jenkinsHelper.copyGlobalLibraryScript('test.sh')
-		// 				script.sh "bash test.sh ${script.env.BRANCH_NAME} working"
-		// 			}
-		// 			else {
-		// 				script.currentBuild.result = "ABORTED"
-		// 				script.error "Lead aborted this job"
-		// 			}
-		// 		}
-		// 	}
-		// }
 }
