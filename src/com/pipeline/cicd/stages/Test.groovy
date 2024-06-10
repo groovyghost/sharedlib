de8a24bd056e9dd33f9ce10fd3373f0c0e91d3b3
Rahul <grovyghost@protonmail.com>
2024-06-10 12:10:06 +0530

    add qaapproval

--- src/com/pipeline/cicd/stages/Test.groovy
+++ src/com/pipeline/cicd/stages/Test.groovy
@@ -14,9 +14,22 @@ public class Test extends AbstractStage {
 		script.node("${Constant.NODE}") {
 			script.stage(stageName) {
 			jenkinsHelper.copyGlobalLibraryScript('test.sh')
-  			script.sh "bash test.sh ${script.env.BRANCH_NAME} working"
+			script.timeout(time: 5, unit: 'DAYS') {
+				leadApprovalComment = script.input id: 'approve_for_qa', message: 'Approve for Production Deployment', ok:
+                'Proceed' , parameters: 
+				[ script.choice(name: 'Deploy Production Server?', choices: ["Master", "Hotfix", "Abort"].join
+				("\n"), description: 'Build Deploy from stage/hotfix branch')] ,
+				submitter: 'rahul@contus.in'
+				}
+			script.echo("Approval Comment: ${leadApprovalComment}");
+            if (leadApprovalComment.contains("Abort")) {
+                script.currentBuild.result = "ABORTED"
+                script.error "Lead aborted this job"
+            	}
+            else {
+			script.sh "bash test.sh ${script.env.BRANCH_NAME} working"
+				}
 			}
 		}
 	}
 }
-
