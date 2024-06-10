c8a20a07454198ca0c50665b87b399dca4093f5c
Rahul <grovyghost@protonmail.com>
2024-06-10 16:34:37 +0530

    test

--- src/com/pipeline/cicd/Pipeline.groovy
+++ src/com/pipeline/cicd/Pipeline.groovy
@@ -1,26 +1,15 @@
 package com.pipeline.cicd
 
-import com.pipeline.cicd.Constant
 import com.pipeline.cicd.helpers.Exception
 import com.pipeline.cicd.helpers.JenkinsHelper
-import com.pipeline.cicd.stages.*
-import org.jenkinsci.plugins.workflow.cps.DSL
+import com.pipeline.cicd.stages.Stage
 
 class Pipeline implements Serializable {
 
-    def script
-
-    def stages = []
-
-    DSL steps
-
-    JenkinsHelper jenkinsHelper
-
-    static boolean isAndroid = false
-
-    static boolean isIOS = false
-
-    static boolean isDebug = false
+    private final script
+    private final List<Stage> stages = []
+    private final DSL steps
+    private final JenkinsHelper jenkinsHelper
 
     static builder(script, DSL steps) {
         return new Builder(script, steps)
@@ -28,48 +17,37 @@ class Pipeline implements Serializable {
 
     static class Builder implements Serializable {
 
-        def stages = []
+        private final script
+        private final DSL steps
+        private final JenkinsHelper jenkinsHelper
+        private final List<Stage> stages = []
 
-        def script
-
-        def approvalQAComment
-
-        DSL steps
-
-        JenkinsHelper jenkinsHelper
-
-        def PROJECT_REPO_BRANCH;
-
-        Builder(def script, DSL steps) {
+        Builder(script, DSL steps) {
             this.script = script
             this.steps = steps
             this.jenkinsHelper = new JenkinsHelper(script)
-            this.PROJECT_REPO_BRANCH = "${this.script.env.BRANCH_NAME}"
         }
 
-        def withPreparationStage() {
+        Builder withPreparationStage() {
             stages << new Preparation(script, jenkinsHelper)
             return this
         }
-        def withTestStage() {
+
+        Builder withTestStage() {
             stages << new Test(script, jenkinsHelper)
             return this
         }
 
-        def PipelineBuild() {
-                withPreparationStage()
-                withTestStage()
-            return new Pipeline(this)
+        Pipeline build() {
+            return new Pipeline(script, stages, steps, jenkinsHelper)
         }
-
-
     }
 
-    private Pipeline(Builder builder) {
-        this.script = builder.script
-        this.stages = builder.stages
-        this.steps = builder.steps
-        this.jenkinsHelper = builder.jenkinsHelper
+    private Pipeline(script, List<Stage> stages, DSL steps, JenkinsHelper jenkinsHelper) {
+        this.script = script
+        this.stages = stages
+        this.steps = steps
+        this.jenkinsHelper = jenkinsHelper
     }
 
     void execute() {
