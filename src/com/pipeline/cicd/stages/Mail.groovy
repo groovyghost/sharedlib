2a8ca409e830b522ffdc869ac21be2f573898681
Rahul <grovyghost@protonmail.com>
2024-06-11 18:20:49 +0530

    add mailext

--- src/com/pipeline/cicd/stages/Mail.groovy
+++ src/com/pipeline/cicd/stages/Mail.groovy
@@ -15,7 +15,8 @@ public class Test2 extends AbstractStage {
                 script.node(Constant.NODE) {
                 String scriptpath = jenkinsHelper.copyGlobalLibraryScript('test.sh')
                 script.sh "bash ${scriptpath} ${script.env.BRANCH_NAME} working"
-                script.mail(body: 'Body for mail', cc: 'rahul.a@contus.in', from: 'Jenkins', subject: 'Test subject', to: 'rahula7200@gmail.com')
+                //script.mail(body: 'Body for mail', cc: 'rahul.a@contus.in', from: 'Jenkins', subject: 'Test subject', to: 'rahula7200@gmail.com')
+                script.emailext(body: 'Body for mail', cc: 'rahul.a@contus.in', from: 'Jenkins', subject: 'Test subject', to: 'rahula7200@gmail.com')
             }
         }
     }
