7e0ff6df279f77e8d771ab7e144b46a6f6918599
Rahul <grovyghost@protonmail.com>
2024-06-19 16:30:53 +0530

    minor changes

--- src/com/pipeline/cicd/stages/UAT/UATDeployment.groovy
+++ src/com/pipeline/cicd/stages/UAT/UATDeployment.groovy
@@ -12,25 +12,35 @@ public class UATDeployment extends AbstractStage {
 
     @Override
     void execute() {
-        script.timeout(time: 1, unit: 'DAYS') {
-            String deployChoice = script.input id: 'approveForDeployment', message: 'Approve for deployment',
-                    parameters: [script.choice(name: 'Deploy to Staging?', choices: 'Yes\nNo')], submitter: Constant.AUTHOR
-            script.stage(stageName) {
-                script.node(Constant.NODE) {
-                    def buildProperties = new BuildProperties(script)
-                    buildProperties.readBuildProperties()
-                    if (deployChoice == 'Yes') {
-                        String scriptPath = jenkinsHelper.copyGlobalLibraryScript("deploy-docker.sh")
-                        script.sh("bash ${scriptPath} ${Constant.SERVICE_NAME}")
-                    } else if (deployChoice == 'No'){
-                        script.currentBuild.result = "ABORTED"
-                        script.error "Lead aborted this job"
-                    } else {
-                        script.error "Invalid input"
+        try {
+            script.timeout(time: 1, unit: 'DAYS') {
+                String deployChoice = script.input(
+                        id: 'approveForDeployment',
+                        message: 'Approve for deployment',
+                        parameters: [script.choice(name: 'Deploy to Staging?', choices: ['Yes', 'No'])],
+                        submitter: Constant.AUTHOR
+                )
+
+                script.stage(stageName) {
+                    script.node(Constant.NODE) {
+                        def buildProperties = new BuildProperties(script)
+                        buildProperties.readBuildProperties()
+
+                        if (deployChoice == 'Yes') {
+                            String scriptPath = jenkinsHelper.copyGlobalLibraryScript("deploy-docker.sh")
+                            script.sh("bash ${scriptPath} ${Constant.SERVICE_NAME}")
+                        } else if (deployChoice == 'No') {
+                            script.currentBuild.result = "ABORTED"
+                            script.error "Lead aborted this job"
+                        } else {
+                            throw new IllegalArgumentException("Invalid input: ${deployChoice}")
+                        }
                     }
+                }
             }
+        } catch (Exception e) {
+            script.currentBuild.result = "FAILURE"
+            script.error "Build failed: ${e.toString()}"
         }
     }
 }
-//script.withEnv(["BRANCH_NAME=${script.env.BRANCH_NAME}","MYENV=rahul"]){}
-
