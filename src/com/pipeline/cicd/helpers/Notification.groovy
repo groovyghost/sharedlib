f4190fb50c13dfc14c2e423853b8f9184e987d41
Rahul <grovyghost@protonmail.com>
2024-06-19 18:10:07 +0530

    minor fix

--- src/com/pipeline/cicd/helpers/Notification.groovy
+++ src/com/pipeline/cicd/helpers/Notification.groovy
@@ -1,13 +1,20 @@
-        private void sendNotification(String status) {
-            String emailContent = jenkinsHelper.getEmailContent("Staging")
-            script.mail(
-                from: 'rahul.a@contus.in',
-                to: 'rahula7200@gmail.com',
-                cc: 'rahul.a@contus.in',
-                subject: '🔵 $PROJECT_NAME - Build # $BUILD_NUMBER - ' + status,
-                body: emailContent
-            )
+package com.pipeline.cicd.helpers
+
+import com.pipeline.cicd.helpers.JenkinsHelper
+
+class Notification implements Serializable {
+    void sendNotification(String status) {
+        String emailContent = jenkinsHelper.getEmailContent("Staging")
+        String from = 'rahul.a@contus.in'
+        String to = 'rahula7200@gmail.com'
+        String cc = 'rahul.a@contus.in'
+        String subject = "🔵 $PROJECT_NAME - Build # $BUILD_NUMBER - $status"
+
+        if (from == null || to == null || cc == null || subject == null || emailContent == null) {
+            throw new IllegalStateException("One or more required parameters are null")
         }
 
-
+        script.mail(from: from, to: to, cc: cc, subject: subject, body: emailContent)
+    }
+}
 
