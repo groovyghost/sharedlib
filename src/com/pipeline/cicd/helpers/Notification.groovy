87eba4311f8a6807584f4762b8ce8fa2ef0413c1
Rahul <grovyghost@protonmail.com>
2024-06-22 22:08:22 +0530

    test

--- src/com/pipeline/cicd/helpers/Notification.groovy
+++ src/com/pipeline/cicd/helpers/Notification.groovy
@@ -1,6 +1,7 @@
 package com.pipeline.cicd.helpers
 
 import com.pipeline.cicd.Constant
+import java.lang.NullPointerException
 
 class Notification implements Serializable {
 
@@ -9,21 +10,32 @@ class Notification implements Serializable {
     Notification(Object script) {
         this.script = script
     }
+
     String convertHTMLToString(def filename) {
-		def emailContent = script.readFile encoding: 'UTF-8', file: "${filename}"
-		return emailContent;
-	}
+        if (filename == null) {
+            throw new NullPointerException("Filename cannot be null")
+        }
+        def emailContent = script.readFile encoding: 'UTF-8', file: "${filename}"
+        return emailContent;
+    }
 
     String getEmailContent() {
-		convertHTMLToString("email_template.html")
+        def emailContent = convertHTMLToString("email_template.html")
+        if (emailContent == null) {
+            throw new NullPointerException("emailContent cannot be null")
+        }
+        return emailContent
                 .replace('${subject}', "Pipeline Failed: ${script.env.JOB_NAME} ${script.env.BUILD_NUMBER}")
                 .replace('${jobName}', script.env.JOB_NAME)
                 .replace('${buildNumber}', script.env.BUILD_NUMBER)
                 .replace('${content}', "Pipeline ${script.env.JOB_NAME} build ${script.env.BUILD_NUMBER} failed.")
                 .replace('${buildUrl}', script.env.BUILD_URL);
-	}
+    }
 
     def sendMailNotification() {
+        if (Constant.OPS_MAIL == null) {
+            throw new NullPointerException("OPS_MAIL cannot be null")
+        }
         script.emailext(
             to: Constant.OPS_MAIL,
             subject: "Pipeline Failed: ${script.env.JOB_NAME} ${script.env.BUILD_NUMBER}",
