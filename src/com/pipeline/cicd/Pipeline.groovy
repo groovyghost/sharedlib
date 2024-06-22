6c7ed9a9b5ba2d4215b8cf62690bc90f20a49157
Rahul <grovyghost@protonmail.com>
2024-06-22 18:53:31 +0530

    test

--- src/com/pipeline/cicd/Pipeline.groovy
+++ src/com/pipeline/cicd/Pipeline.groovy
@@ -9,62 +9,36 @@ import org.jenkinsci.plugins.workflow.cps.DSL
 class Pipeline implements Serializable {
 
     def script
-
     def stages = []
-
     DSL steps
-
-
     JenkinsHelper jenkinsHelper
+    def PROJECT_REPO_BRANCH
 
-    static builder(script, DSL steps) {
-        return new Builder(script, steps)
+    Pipeline(def script, DSL steps) {
+        this.script = script
+        this.steps = steps
+        this.jenkinsHelper = new JenkinsHelper(script)
+        this.PROJECT_REPO_BRANCH = "${this.script.env.BRANCH_NAME}"
     }
 
-    static class Builder implements Serializable {
-
-        def stages = []
-
-        def script
-
-        DSL steps
-
-        JenkinsHelper jenkinsHelper
-
-        def PROJECT_REPO_BRANCH;
-
-        Builder(def script, DSL steps) {
-            this.script = script
-            this.steps = steps
-            this.jenkinsHelper = new JenkinsHelper(script)
-            this.PROJECT_REPO_BRANCH = "${this.script.env.BRANCH_NAME}"
+    static Pipeline PipelineBuild(def script, DSL steps) {
+        Pipeline pipeline = new Pipeline(script, steps)
+        pipeline.withPreparationStage()
+        if (pipeline.PROJECT_REPO_BRANCH.toLowerCase() == "staging") {
+            Constant.NODE = "agent2"
+            pipeline.withCleanupStage()
         }
-
-        def withPreparationStage() {
-            stages << new Preparation(script, jenkinsHelper)
-            return this
-        }
-
-        def withCleanupStage() {
-            stages << new Cleanup(script, jenkinsHelper)
-            return this
-        }
-
-        def PipelineBuild() {
-            withPreparationStage()
-            if (PROJECT_REPO_BRANCH.toLowerCase() == "staging"){
-                Constant.NODE = "agent2"
-                withCleanupStage()
-            }
-            return new Pipeline(this)
-        }
+        return pipeline
     }
 
-    private Pipeline(Builder builder) {
-        this.script = builder.script
-        this.stages = builder.stages
-        this.steps = builder.steps
-        this.jenkinsHelper = builder.jenkinsHelper
+    Pipeline withPreparationStage() {
+        stages << new Preparation(script, jenkinsHelper)
+        return this
+    }
+
+    Pipeline withCleanupStage() {
+        stages << new Cleanup(script, jenkinsHelper)
+        return this
     }
 
     void execute() {
@@ -72,10 +46,9 @@ class Pipeline implements Serializable {
             try {
                 stage.execute()
             } catch (err) {
-                throw err
                 new Exception(script).handle(err)
+                throw err
             }
         }
     }
 }
-
