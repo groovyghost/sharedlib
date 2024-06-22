943dddc1b72b77b3b0d511ddfff7aadfdffe050d
Rahul <grovyghost@protonmail.com>
2024-06-22 18:00:52 +0530

    test

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -5,7 +5,14 @@ import org.jenkinsci.plugins.workflow.cps.DSL
 class PipelineBuild implements Serializable {
 
     def execute() {
-        script.echo "Executing pipeline build"
-        steps.sh "echo 'Hello, world!'"
+    node {
+    stage('Example') {
+        if (env.BRANCH_NAME == 'master') {
+            echo 'I only execute on the master branch'
+        } else {
+            echo 'I execute elsewhere'
+        }
+    }
+}
    }
 }
