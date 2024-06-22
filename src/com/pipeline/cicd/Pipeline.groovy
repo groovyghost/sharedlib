f28f6a5ec528f20fcedb6dd4008df64c84eee10d
Rahul <grovyghost@protonmail.com>
2024-06-22 18:47:17 +0530

    test

--- src/com/pipeline/cicd/Pipeline.groovy
+++ src/com/pipeline/cicd/Pipeline.groovy
@@ -3,69 +3,28 @@ package com.pipeline.cicd
 import com.pipeline.cicd.Constant
 import com.pipeline.cicd.helpers.Exception
 import com.pipeline.cicd.helpers.JenkinsHelper
-import com.pipeline.cicd.stages.*
-import com.pipeline.cicd.stages.UAT.*
+import com.pipeline.cicd.stages.Stage
 import org.jenkinsci.plugins.workflow.cps.DSL
 
 class Pipeline implements Serializable {
 
     def script
 
-    def stages = []
+    List<Stage> stages = []
 
     DSL steps
 
-
     JenkinsHelper jenkinsHelper
 
-    static builder(script, DSL steps) {
-        return new Builder(script, steps)
-    }
-
-    static class Builder implements Serializable {
-
-        def stages = []
-
-        def script
-
-        DSL steps
-
-        JenkinsHelper jenkinsHelper
-
-        def PROJECT_REPO_BRANCH;
-
-        Builder(def script, DSL steps) {
-            this.script = script
-            this.steps = steps
-            this.jenkinsHelper = new JenkinsHelper(script)
-            this.PROJECT_REPO_BRANCH = "${this.script.env.BRANCH_NAME}"
+    Pipeline(script, DSL steps) {
+        this.script = script
+        this.steps = steps
+        this.jenkinsHelper = new JenkinsHelper(script)
+        this.stages << new Preparation(script, jenkinsHelper)
+        if (script.env.BRANCH_NAME.toLowerCase() == "staging"){
+            Constant.NODE = "agent2"
+            this.stages << new Cleanup(script, jenkinsHelper)
         }
-
-        def withPreparationStage() {
-            stages << new Preparation(script, jenkinsHelper)
-            return this
-        }
-
-        def withCleanupStage() {
-            stages << new Cleanup(script, jenkinsHelper)
-            return this
-        }
-
-        def PipelineBuild() {
-            withPreparationStage()
-            if (PROJECT_REPO_BRANCH.toLowerCase() == "staging"){
-                Constant.NODE = "agent2"
-                withCleanupStage()
-            }
-            return new Pipeline(this)
-        }
-    }
-
-    private Pipeline(Builder builder) {
-        this.script = builder.script
-        this.stages = builder.stages
-        this.steps = builder.steps
-        this.jenkinsHelper = builder.jenkinsHelper
     }
 
     void execute() {
