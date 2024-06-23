08b6f00ca2d69f68f7a9b041fc33e5bbd9e60012
Rahul <grovyghost@protonmail.com>
2024-06-23 13:07:22 +0530

    test

--- resources/templates/email.html.groovy
+++ resources/templates/email.html.groovy
@@ -5,13 +5,112 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pipeline Notification</title>
-    <link rel="stylesheet" href="./email.css">
+    <style>
+        body {
+            background-color: #f5f5f5;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            font-family: sans-serif;
+        }
+
+        .container {
+            background-color: white;
+            width: 600px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin: 20px;
+            text-align: center;
+            font-family: sans-serif;
+        }
+
+        .icon {
+            padding: 20px;
+            border-bottom: 1px solid #ddd;
+        }
+
+        .icon img {
+            width: auto;
+            height: auto;
+        }
+
+        .header {
+            padding: 20px;
+            font-size: 18px;
+            border-bottom: 1px solid #ddd;
+        }
+
+        .header.SUCCESS {
+            background-color: #28a745;
+            color: white;
+        }
+
+        .header.FAILURE {
+            background-color: #dc3545;
+            color: white;
+        }
+
+        .content {
+            padding: 20px;
+            text-align: left;
+            font-family: sans-serif;
+        }
+
+        .content table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 20px;
+        }
+
+        .content table td {
+            padding: 10px;
+            border-bottom: 1px solid #f5f5f5;
+            font-family: sans-serif;
+        }
+
+        .content table .label {
+            font-weight: bold;
+            width: 30%;
+            color: #333;
+        }
+
+        .content table td.bold-text {
+            font-weight: bold;
+        }
+
+        .pipeline-url {
+            text-align: center;
+            margin-bottom: 10px;
+            font-family: sans-serif;
+        }
+
+        .pipeline-footer {
+            font-family: sans-serif;
+            text-align: center;
+        }
+
+        .pipeline-url a {
+            color: #007bff;
+            text-decoration: none;
+        }
+
+        .footer {
+            text-align: center;
+            padding: 10px 20px;
+            color: #666;
+            font-size: 12px;
+            background-color: #f5f5f5;
+            border-top: 1px solid #ddd;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 
 <body>
     <div class="container">
         <div class="icon">
-            <img id="icon" src="./jenkins.svg" alt="Jenkins Icon">
+            <img id="icon" src="https://www.jenkins.io/images/logos/jenkins/jenkins.svg" alt="Jenkins Icon">
         </div>
         <div id="header" class="header SUCCESS">
             <span id="header-text"></span>
@@ -58,13 +157,13 @@
                 headerElement.classList.remove('FAILURE');
                 headerElement.classList.add('SUCCESS');
                 headerTextElement.textContent = '✔ Your pipeline has succeeded!';
-                iconImg.src = './jenkins-success.svg';
+                iconImg.src = 'https://i.postimg.cc/SYNtdkFB/jenkins-success.png';
                 pipelineUrlElement.innerHTML = 'Pipeline #${BUILD_NUMBER} triggered by ${COMMIT_AUTHOR} has been Succeeded.';
             } else {
                 headerElement.classList.remove('SUCCESS');
                 headerElement.classList.add('FAILURE');
                 headerTextElement.textContent = '✖ Your pipeline has failed!';
-                iconImg.src = './jenkins-failed.svg';
+                iconImg.src = 'https://i.postimg.cc/PpMR1Ncb/jenkins-failed.png';
                 pipelineUrlElement.innerHTML = 'Pipeline #${BUILD_NUMBER} triggered by ${COMMIT_AUTHOR} has Failed.';
             }
         });
